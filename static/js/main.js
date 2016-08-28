1cf93bee1de9f8e3e064bbbae1a3a476ebd89bcf
Sara <dibartolomeo.sara@gmail.com>
2016-08-28 16:23:47 +0200

    replaced Babylon with three.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
-var camera;
-var scene;
+var camera, scene, renderer, controls, cube;
+
 
 function tilt(data){
 	document.getElementById("thing").innerHTML = "alpha: " + data[0] + " beta: " + data[1] + " gamma: " + data[2];
@@ -20,48 +20,43 @@ if (window.DeviceOrientationEvent) {
     }, true);
 }
 
-window.addEventListener('DOMContentLoaded', function(){
-            var canvas = document.getElementById('renderCanvas');
-            var engine = new BABYLON.Engine(canvas, true);
+function render() { 
+    requestAnimationFrame( render );
+    controls.update(); 
+    cube.rotation.x += 0.1; 
+    cube.rotation.y += 0.1;
+    renderer.render( scene, camera ); 
+} 
 
-            var createScene = function(){
-                var scene = new BABYLON.Scene(engine);
+function init(){
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+    scene = new THREE.Scene();
 
-                var camera = new BABYLON.DeviceOrientationCamera("DevOr_camera", new BABYLON.Vector3(0, 1, -15), scene);
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
-                camera.setTarget(BABYLON.Vector3.Zero());
-                camera.attachControl(canvas, false);
+    controls = new DeviceOrientationController( camera, renderer.domElement );
+    controls.connect();
+    //setupControllerEventHandlers( controls );
+    window.addEventListener( 'resize', onWindowResize, false );
 
-                var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
+    cube = new THREE.Mesh( geometry, material ); 
+    scene.add( cube ); 
+    camera.position.z = 5;
 
-                var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-                sphere.position.y = 1;
+    render();
+}
 
-                var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
-                /*var plane = BABYLON.Mesh.CreatePlane("plane", 10, scene);
-                plane.position.x = 10;
-                var materialPlane = new BABYLON.StandardMaterial("texturePlane", scene);
-                materialPlane.diffuseTexture = new BABYLON.Texture("Pikachu.png", scene);
-                materialPlane.backFaceCulling = false;
-                plane.material = materialPlane;*/
 
-                var skyMaterial = new BABYLON.SkyMaterial("skyMaterial", scene);
-                skyMaterial.backFaceCulling = false;
-                skyMaterial.luminance = .5;
 
-                var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
-                skybox.material = skyMaterial;
 
-                return scene;
-            }
-
-            scene = createScene();
-            engine.runRenderLoop(function(){
-                scene.render();
-            });
-
-            window.addEventListener('resize', function(){
-                engine.resize();
-            });
-        });
+init();
