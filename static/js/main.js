6ee1c997e97be20b54fdc9b46a4d29ed46cf73a8
Sara <dibartolomeo.sara@gmail.com>
2016-08-29 09:01:26 +0200

    skybox

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-var camera, scene, renderer, controls, cube;
+var camera, scene, renderer, controls, cube, texture_placeholder, container;
 
 
 function tilt(data){
@@ -29,17 +29,42 @@ function render() {
 } 
 
 function init(){
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
     scene = new THREE.Scene();
 
-    renderer = new THREE.WebGLRenderer();
+    container = document.getElementById( 'container' );
+    texture_placeholder = document.createElement( 'canvas' );
+	texture_placeholder.width = 128;
+	texture_placeholder.height = 128;
+	
+	var context = texture_placeholder.getContext( '2d' );
+	context.fillStyle = 'rgb( 200, 200, 200 )';
+	context.fillRect( 0, 0, texture_placeholder.width, texture_placeholder.height );
+
+    //renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.CanvasRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    //document.body.appendChild( renderer.domElement );
+    container.appendChild(renderer.domElement);
 
     controls = new DeviceOrientationController( camera, renderer.domElement );
     controls.connect();
     //setupControllerEventHandlers( controls );
     window.addEventListener( 'resize', onWindowResize, false );
+
+    //CREATE SKYBOX
+    var materials = [
+		loadTexture( '/static/assets/skybox/skybox_px.jpg' ), // right
+		loadTexture( '/static/assets/skybox/skybox_nx.jpg' ), // left
+		loadTexture( '/static/assets/skybox/skybox_py.jpg' ), // top
+		loadTexture( '/static/assets/skybox/skybox_ny.jpg' ), // bottom
+		loadTexture( '/static/assets/skybox/skybox_pz.jpg' ), // back
+		loadTexture( '/static/assets/skybox/skybox_nz.jpg' )  // front
+	];
+	var mesh = new THREE.Mesh( new THREE.BoxGeometry( 300, 300, 300, 7, 7, 7 ), new THREE.MultiMaterial( materials ) );
+	mesh.scale.x = - 1;
+	scene.add( mesh );
 
     var geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); 
@@ -54,6 +79,24 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+function loadTexture( path ) {
+
+	var texture = new THREE.Texture( texture_placeholder );
+	var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5, shading:THREE.FlatShading } );
+
+	var image = new Image();
+	image.onload = function () {
+
+		texture.image = this;
+		texture.needsUpdate = true;
+
+	};
+	image.src = path;
+
+	return material;
+
 }
 
 
