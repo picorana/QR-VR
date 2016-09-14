87542fd6a41fe1765cd1043264382583f9f2067c
Sara <dibartolomeo.sara@gmail.com>
2016-09-14 17:46:16 +0200

    just in case

---
+++ servethis.py
@@ -0,0 +1,19 @@
+import SocketServer
+import BaseHTTPServer
+import SimpleHTTPServer
+import sys
+
+class ThreadingSimpleServer(SocketServer.ThreadingMixIn, BaseHTTPServer.HTTPServer): pass
+
+if sys.argv[1:]:
+    port = int(sys.argv[1])
+else:
+    port = 8000
+
+server = ThreadingSimpleServer(('', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
+try:
+    while 1:
+        sys.stdout.flush()
+        server.handle_request()
+except KeyboardInterrupt:
+    print "Finished"
